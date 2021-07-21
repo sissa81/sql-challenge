aeb905878b3aa2791e0f391234ac4a7ad056b63f
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-07-21 01:16:36 -0700

    7/21/21 through 5

---
+++ EmployeeSQL/employee_query.sql
@@ -0,0 +1,55 @@
+--#1 List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no = salaries.emp_no;
+
+--#2 List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name,
+	last_name,
+	hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+
+
+--#3 List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no,
+	departments.dept_name,
+	dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM departments
+INNER JOIN dept_manager ON
+departments.dept_no = dept_manager.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no = employees.emp_no;
+
+--#4 List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no;
+
+--#5 List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name,
+	last_name,
+	sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--#6 List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM employees
+INNER JOIN 
